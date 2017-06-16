b12ef97564e08f832337c6801e147b8c089dd4f8
Koji Murata <koji.murata@dena.com>
2017-06-16 21:44:27 +0900

    carthage

---
+++ Carthage/PictureInPicture/PictureInPicture/PictureInPicture.h
@@ -0,0 +1,19 @@
+//
+//  PictureInPicture.h
+//  PictureInPicture
+//
+//  Created by Koji Murata on 2017/07/19.
+//  Copyright © 2017年 Koji Murata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PictureInPicture.
+FOUNDATION_EXPORT double PictureInPictureVersionNumber;
+
+//! Project version string for PictureInPicture.
+FOUNDATION_EXPORT const unsigned char PictureInPictureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PictureInPicture/PublicHeader.h>
+
+
